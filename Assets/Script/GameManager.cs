ccb542452a2b1aaf3b41bce3781e54220a333f93
LAPTOP-2HQ0J53J\qiuyu <guoqiuyu23@gmail.com>
2021-02-18 18:37:02 -0500

    1

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (score ==2)
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 1);
+            score = 0;
+        }
+    }
+}
